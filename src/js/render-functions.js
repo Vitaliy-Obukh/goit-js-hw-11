a540db49ac3600dd16ec8889ff9adb588cf13a98
Vitaliy Obukh <Psiholog211@gmail.com>
2025-04-01 22:11:14 +0300

    hw11

---
+++ src/js/render-functions.js
@@ -0,0 +1,52 @@
+// Функції інтерфейсу
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captions: true,
+  captionsData: 'alt',
+  captionDelay: 250,
+});
+
+export function createImageMarkup(images) {
+  return images
+    .map(
+      ({
+        webformatURL,
+        largeImageURL,
+        tags,
+        likes,
+        views,
+        comments,
+        downloads,
+      }) => `
+      <li class="gallery-item">
+        <a class="gallery-link" href="${largeImageURL}">
+          <img class="gallery-image" src="${webformatURL}" alt="${tags}" loading="lazy" />
+        </a>
+        <div class="thumb-block">
+          <div class="block"><h2 class="tittle">Likes</h2><p class="amount">${likes}</p></div>
+          <div class="block"><h2 class="tittle">Views</h2><p class="amount">${views}</p></div>
+          <div class="block"><h2 class="tittle">Comments</h2><p class="amount">${comments}</p></div>
+          <div class="block"><h2 class="tittle">Downloads</h2><p class="amount">${downloads}</p></div>
+        </div>
+      </li>`
+    )
+    .join('');
+}
+
+export function updateGallery(images) {
+  const gallery = document.querySelector('.gallery');
+  gallery.innerHTML = createImageMarkup(images);
+  lightbox.refresh(); 
+}
+
+export function showNoResultsMessage(message) {
+  iziToast.error({
+    title: 'Error',
+    message: message,
+    position: 'topRight',
+  });
+}
